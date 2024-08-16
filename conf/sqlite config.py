5bacc36555af9a0815a7f696b6eb93e9d4023253
anurag <agtiwari7@gmail.com>
2024-08-16 12:45:12 +0530

    code inhancement

--- conf/sqlite config.py
+++ conf/sqlite config.py
@@ -1,23 +1,20 @@
 import os 
 import sqlite3
 
-path = "config"
-if os.path.exists(f"{path}/photo/template/user.png"):
-    os.chdir(path)
-    con = sqlite3.connect("software.db")
-    cur = con.cursor()
-
-else:
+path = os.path.join(os.getenv('LOCALAPPDATA'), "Programs", "modal", "config")
+if not os.path.exists(f"{path}/photo/template/user.png"):
+    os.makedirs(path, exist_ok=True)
     os.makedirs(f"{path}/photo/current", exist_ok=True)
     os.makedirs(f"{path}/photo/deleted", exist_ok=True)
     os.makedirs(f"{path}/photo/template", exist_ok=True)
-    os.chdir(path)
-    con = sqlite3.connect("software.db")
-    cur = con.cursor()
-    
-    cur.execute("create table if not exists soft_reg (bus_name varchar(30), bus_contact bigint unique, bus_password varchar(30), valid_till varchar(15), sys_hash varchar(100));")
-    cur.execute("create table if not exists act_key (soft_reg_contact bigint, act_key varchar(50) unique, valid_till varchar(15), sys_hash varchar(100), FOREIGN KEY (soft_reg_contact) REFERENCES soft_reg(bus_contact));")
-    con.commit()
+
+os.chdir(path)
+con = sqlite3.connect("software.db")
+cur = con.cursor()
+
+cur.execute("create table if not exists soft_reg (bus_name varchar(30), bus_contact bigint unique, bus_password varchar(30), valid_till varchar(15), sys_hash varchar(100));")
+cur.execute("create table if not exists act_key (soft_reg_contact bigint, act_key varchar(50) unique, valid_till varchar(15), sys_hash varchar(100), FOREIGN KEY (soft_reg_contact) REFERENCES soft_reg(bus_contact));")
+con.commit()
 
 res = cur.execute("select * from soft_reg")
 for row in res.fetchall():
